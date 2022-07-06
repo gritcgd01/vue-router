1c0f3413e8e2fcec79d3a996f144b94bc3f36268
ธนาวุฒิ  จุลเลศ <TANAWUT.CHU@ad.cgd.go.th>
2022-07-06 18:35:22 +0700

    first commit

---
+++ src/views/HelloWorld.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h2>helloworld</h2>
+    <h2>{{question}} </h2>
+    <input type="text" v-model="question"> <input type="text" v-model="question">
+    <button @click="clear">Reset</button>
+  </div>
+
+</template>
+
+<script>
+export default {
+data() {
+    return {
+      question: '',
+      answer: 'login'
+    }
+  },
+  methods:{
+        clear(){
+               this.question=''
+
+        }
+  }
+}
+</script>
+
+<style>
+
+</style>
